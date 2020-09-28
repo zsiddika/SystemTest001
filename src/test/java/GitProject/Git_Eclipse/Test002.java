efadc1f4ef58af8ccf52dc050dc35ec5d9e2620e
zinnat <zinnat@LAPTOP-EB806OJC>
2020-09-28 13:54:00 -0400

    Second commit

---
+++ src/test/java/GitProject/Git_Eclipse/Test002.java
@@ -0,0 +1,11 @@
+package GitProject.Git_Eclipse;
+
+public class Test002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Test002.");
+
+	}
+
+}
